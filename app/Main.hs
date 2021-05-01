048abb451f3c2e94b4821805413e53654715d373
Åsmund Bryne Retterstøl <retterstol@gmail.com>
2021-05-01 16:08:51 +0200

    Updated parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
 import ParseFile
+import BdbValues
 
 main :: IO ()
-main = print =<< getFile "test.bdb"
+main = testParse
+
+getTest :: BValue
+getTest = BObject [("Test", BObject [
+	("TestTall", BNumber 1),
+	("TestString", BString "test"),
+	("InnerObj", BObject [("InnerList", BList [
+		(BBool True)])])])]
+
+printTest :: IO()
+printTest = print getTest
--- src/ParseFile.hs
+++ src/ParseFile.hs
@@ -2,11 +2,56 @@
 
 module ParseFile where
 
-import Text.Parsec
-import Data.Text
+import Text.ParserCombinators.Parsec
+import BdbValues
 
 getFile :: String -> IO String
 getFile = readFile
 
+readObject :: GenParser Char st [[String]]
+readObject = do
+  types <- many readTypes
+  eof
+  return types
+
+readTypes :: GenParser Char st [String]
+readTypes = do
+  types <- readType
+  eol
+  return types
+
+readType :: GenParser Char st [String]
+readType = do
+  bType <- content
+  bValue <- next
+  return $ bType : bValue
+
+content :: GenParser Char st String
+content = many (noneOf " \n") 
+
+next :: GenParser Char st [String]
+next = (char ' ' >> readType) <|> return []
+
+eol :: GenParser Char st Char
+eol = char '\n'
+  
+parseB :: String -> Either ParseError [[String]]
+parseB = parse readObject "Feil"
+
+getResult :: Either ParseError [[String]] -> IO ()
+getResult parsed = case parsed of
+  Right list -> print $ convertTypes list
+  Left b -> print b
+
+testParse :: IO ()
+testParse = do
+  file <- getFile "test.bdb"
+  getResult $ parseB file
+
+convertTypes :: [[String]] -> [Maybe BValue]
+convertTypes = map (convertStringToBValue . convertListToTup)
+
+convertListToTup :: [String] -> (String, String)
+convertListToTup (x:y:_) = (x,y)
 
 
