2b298e7af9a27f88e9bc096911e51b3af3fc2e65
Åsmund Bryne Retterstøl <retterstol@gmail.com>
2021-09-03 08:27:00 +0200

    stupid

--- src/Index/IndexTypes.hs
+++ src/Index/IndexTypes.hs
@@ -1,6 +1,19 @@
 module Index.IndexTypes where
 
-data BTree a = BTree a 
+import Data.Vector
 
-data BNode
+type Keys k = Vector k
+
+type Values v = Vector v
+
+data Leaf k v = Leaf (Keys k) (Values v) (Maybe (Leaf k v))
+
+type Children k v = Vector (BTree k v)
+
+data Node k v = Node (Keys k) (Children k v)
+
+data BTree k v = Node k v | Leaf k v
+
+newtype BTreeRoot k v = BTreeRoot (BTree k v)
+
 
