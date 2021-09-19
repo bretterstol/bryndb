654250f678fe72ac0dec9549d6ff556d2a23e132
Åsmund Bryne Retterstøl <retterstol@gmail.com>
2021-09-19 13:40:19 +0200

    Starter btree

--- src/Index/Index.hs
+++ src/Index/Index.hs
@@ -1,2 +1,21 @@
-module Index where
+module Index.Index where
 
+import Data.Vector
+import Index.IndexTypes
+
+
+
+keys :: [k] -> Keys k
+keys = fromList
+
+values :: [v] -> Values v
+values = fromList
+
+leaves :: [Leaf k v] -> Children k v
+leaves = fromList . fmap BTreeLeaf
+
+leaf :: Keys k -> Values v -> Maybe (Leaf k v) -> Leaf k v
+leaf = Leaf
+
+node :: Keys k -> Children k v -> BTree k v
+node ks = BTreeNode . Node ks
--- src/Index/IndexTypes.hs
+++ src/Index/IndexTypes.hs
@@ -1,5 +1,5 @@
 module Index.IndexTypes where
-
+--https://stackoverflow.com/questions/20309501/how-to-implement-b-tree-in-haskell
 import Data.Vector
 
 type Keys k = Vector k
@@ -12,7 +12,7 @@ type Children k v = Vector (BTree k v)
 
 data Node k v = Node (Keys k) (Children k v)
 
-data BTree k v = Node k v | Leaf k v
+data BTree k v = BTreeNode (Node k v) | BTreeLeaf (Leaf k v)
 
 newtype BTreeRoot k v = BTreeRoot (BTree k v)
 
