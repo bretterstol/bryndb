cbcdb960f75b3fab564d31d5fc367ac22fb2ec28
Åsmund Bryne Retterstøl <retterstol@gmail.com>
2021-10-12 00:11:48 +0200

    Working hard :)

--- src/BTree/BTree.hs
+++ src/BTree/BTree.hs
@@ -12,15 +12,15 @@ createTree key val = Leaf [Value (key, [val])]
 insert :: (Ord k) => k -> v -> BTree k v -> BTree k v
 insert key val (Leaf values) =
   let vlenght = length values
-      newValues = newValue : values
+      newValues = sort $ newValue : values
       newValue = Value (key, [val])
-  in if vlenght < treeSize then Leaf $ sort newValues
+  in if vlenght <= treeSize then Leaf $ newValues
     else let Value (medianValue, _) = getMedianValue newValues
-         in Node [medianValue] [lowerLeafs medianValue newValues, higherLeafs medianValue newValues]
-insert key val (Node keys children) =
+         in Node [medianValue] [lowerLeafs medianValue newValues, higherLeafs medianValue newValues] 1
+insert key val (Node keys children size) =
   let index = getChildIndex key keys
       child = children !! index
-  in refactor keys $ map (\c -> if c == child then insert key val child else c) children
+  in refactor keys (map (\c -> if c == child then insert key val child else c) children) size
 
 
 lowerLeafs :: (Ord a) => a -> [Value a b] -> BTree a b
@@ -30,16 +30,18 @@ higherLeafs :: (Ord a) => a -> [Value a b] -> BTree a b
 higherLeafs key values = Leaf $ filterValues (> key) values
 
 filterValues :: (a -> Bool) -> [Value a b] -> [Value a b]
-filterValues func = filter (\(Value (a, _)) -> func a)
+filterValues func =  filter (\(Value (a, _)) -> func a)
 
 getMedianValue :: (Ord a) => [Value a b] -> Value a b
-getMedianValue values = sort values !! getMedianValueIndex values
+getMedianValue values
+  | odd (length values) && (length values > 1) = sort values !!(getMedianIndex values + 1)
+ | otherwise = sort values !! getMedianIndex values
 
-getMedianValueIndex :: [Value a b] -> Int
-getMedianValueIndex = divideByTwo . length
+getMedianIndex :: [a] -> Int
+getMedianIndex = divideByTwo . length
 
 getMedian :: (Ord a) => [a] -> a
-getMedian keys = sort keys !! (length keys `div` 2)
+getMedian keys = sort keys !! getMedianIndex keys
 
 divideByTwo :: Int -> Int
 divideByTwo val = val `div` 2
@@ -49,25 +51,34 @@ getChildIndex key keys = case safeHead (getChildIndexes key keys) of
   Just a -> a
   Nothing -> length keys
 
+
 getChildIndexes :: (Ord k) => k -> [k] -> [Int]
 getChildIndexes key = findIndices (key <=)
 
-refactor :: (Ord a) => [a] -> [BTree a b] -> BTree a b
-refactor keys children
-  | allSameType children = Node keys children
-  | otherwise = refactorUpwords $ refactorInner keys children
+refactor :: (Ord a) => [a] -> [BTree a b] -> Int -> BTree a b
+refactor keys children size
+  | all isLeaf children = Node keys children size
+  | not $ sameHeightChildren children = refactorUpwords $ refactorInner keys children
+  | otherwise = refactorUpwords $ Node keys children size
 
+sameHeightChildren ::(Eq a) => [BTree a b] -> Bool
+sameHeightChildren children = all ((== nodeOr0 (head children)) . nodeOr0) children
+
+nodeOr0 :: BTree a b -> Int
+nodeOr0 tree = case tree of
+  Node _ _ s -> s
+  _ -> 0
 
 refactorUpwords :: (Ord a) => BTree a b -> BTree a b
 refactorUpwords leaf@(Leaf _) = leaf
-refactorUpwords node@(Node keys values)
+refactorUpwords node@(Node keys values height)
   | length keys <= treeSize = node
   | otherwise =
     let median = getMedian keys
     in Node [median] [
-    Node (getLowerKeys median keys) (filter filterEmpty $ map (getLower median) values),
-    Node (getHigherKeys median keys) (filter filterEmpty $ map (getHigher median) values)
-    ]
+    Node (getLowerKeys median keys) (filter filterEmpty $ map (getLower median) values) height,
+    Node (getHigherKeys median keys) (filter filterEmpty $ map (getHigher median) values) height
+    ] (height + 1)
 
 
 getLowerKeys :: (Ord a) => a -> [a] -> [a]
@@ -78,12 +89,12 @@ getHigherKeys median = filter (> median)
 
 getLower :: (Ord a) => a -> BTree a b -> BTree a b
 getLower key tree = case tree of
-  Node a b -> Node (getLowerKeys key a) (map (getLower key) b)
+  Node a b s -> Node (getLowerKeys key a) (map (getLower key) b) s
   Leaf a -> Leaf (filterValues (<= key) a)
 
 getHigher :: (Ord a) => a -> BTree a b -> BTree a b
 getHigher key tree = case tree of
-  Node a b -> Node (getHigherKeys key a) (map (getHigher key) b)
+  Node a b s -> Node (getHigherKeys key a) (map (getHigher key) b) s
   Leaf a -> Leaf (filterValues (> key) a)
 
 filterEmpty :: (Ord a) => BTree a b -> Bool
@@ -93,12 +104,35 @@ filterEmpty tree = case tree of
 
 refactorInner ::(Ord a) => [a] -> [BTree a b] -> BTree a b
 refactorInner keys children =
-  let  (Node innK innC) = getInnerNode children
-       leafs = getInnerLeafs children
-  in Node (sort $ keys ++ innK) (sort $ innC ++ leafs)
+  let  node@(Node innK innC size) = getBiggestTree children
+       leafs = filter (/= node) children
+  in Node (sort $ keys ++ innK) (sort $ innC ++ leafs) size
 
 getInnerNode :: [BTree a b] -> BTree a b
 getInnerNode children = head $ getInnerNodes children
+
+
+getBiggestTree :: [BTree a b] -> BTree a b
+getBiggestTree tree = 
+  let maxHeight = heighest 0 tree
+  in case find (treeHasSize maxHeight) tree of
+    Just t -> t
+    Nothing -> getInnerNode tree
+
+treeHasSize :: Int -> BTree a b -> Bool
+treeHasSize s tree = case tree of 
+  Leaf _ -> False   
+  Node _ _ hs -> s == hs
+
+heighest :: Int -> [BTree a b] -> Int
+heighest m tree 
+  | null tree = m
+  | otherwise = heighest (highestHeight m (head tree)) (tail tree)
+
+highestHeight :: Int -> BTree a b -> Int
+highestHeight maxSoFar tree = case tree of
+  Leaf _ -> maxSoFar
+  Node _ _ height -> if height > maxSoFar then  height else maxSoFar
 
 getInnerNodes :: [BTree a b] -> [BTree a b]
 getInnerNodes = filter isNode
@@ -108,7 +142,7 @@ getInnerLeafs = filter isLeaf
 
 isNode :: BTree a b -> Bool
 isNode tree = case tree of
-  Node _ _ -> True
+  Node {} -> True
   _ -> False
 
 isLeaf :: BTree a b -> Bool
@@ -121,5 +155,5 @@ allSameType children = all (sameType (head children)) children
 
 sameType :: BTree a b -> BTree a b -> Bool
 sameType (Leaf _) (Leaf _) = True
-sameType (Node _ _) (Node _ _) = True
+sameType Node {} Node {} = True
 sameType _ _ = False
--- src/BTree/BTreeTypes.hs
+++ src/BTree/BTreeTypes.hs
@@ -3,7 +3,7 @@ module BTree.BTreeTypes where
 
 newtype Value a b = Value (a, [b]) deriving Show
 
-data BTree a b = Leaf [Value a b] | Node [a] [BTree a b] deriving (Show)
+data BTree a b = Leaf [Value a b] | Node [a] [BTree a b]  Int deriving (Show)
 
 -- BTreeRoot Size Height BTree
 data BTreeRoot a b = BTreeRoot {
@@ -21,11 +21,11 @@ instance (Ord a) => Ord (Value a b) where
 
 instance (Eq a) => Eq (BTree a b) where
   (Leaf a1) == (Leaf a2) = a1 == a2
-  (Node a1 _) == (Node a2 _) = a1 == a2
-  Leaf _ == Node _ _ = False
-  Node _ _ == Leaf _ = False
+  (Node a1 a2 a3) == (Node b1 b2 b3) = a1 == b1 && a2 == b2 && a3 == b3
+  Leaf _ == Node {}= False
+  Node {} == Leaf _ = False
 
 instance (Ord a) => Ord (BTree a b) where
   (Leaf a1) `compare` (Leaf a2) = a1 `compare` a2
-  (Node a1 _) `compare` (Node a2 _) = a1 `compare` a2
+  (Node a1 _ _) `compare` (Node a2 _ _) = a1 `compare` a2
   _ `compare` _ = EQ
