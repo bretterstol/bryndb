87f5e67373e02b4c79eb38bacdd5915ad3bf256c
Åsmund Bryne Retterstøl <retterstol@gmail.com>
2022-01-05 22:45:31 +0100

    Fixing the dummy

--- app/Main.hs
+++ app/Main.hs
@@ -8,14 +8,13 @@ import Control.Concurrent.STM
 test :: BValue
 test = BMap [("ny", BString "hie"),("detteERKult", BNumber 1),("nes", BMap [("enda en", BString "hei på feg")])]
 
-tree :: BTree Int String
-tree = B.insert 3 "seg" (B.createTree 2 "hei")
+test2 :: BValue
+test2 = BMap [("ny", BString "enda lenger greie"),("detteERKult", BNumber 10),("nes", BMap [("ny", BString "hei på feg")])]
 
-
-indexStore :: IO (TVar [(String, BTree Int String)])
+indexStore :: IO (TVar [(String, BTree a b)])
 indexStore = newTVarIO []
 
-updateIndexStore ::  String -> BTree Int String -> [(String, BTree Int String)]-> [(String, BTree Int String)]
+updateIndexStore :: (Ord a) =>  String -> BTree a b -> [(String, BTree a b)]-> [(String, BTree a b)]
 updateIndexStore name test store = (name, test) : store
 
 creatingADummyIndex :: BTree Int String
@@ -32,13 +31,15 @@ creatingADummyIndex = B.insert 4 "halla" $
    B.insert 15 "ytreyrey" $
    B.insert 22 "yht" $
    B.insert (-1) "feweer" $
-  B.insert 31 "fqweqwe"  $
-  B.insert 43 "ytreyrey" $
-  B.insert 54 "ytreyrey" $
-  B.insert 30 "ytreyrey" $
-  B.insert 17 "ytreyrey" $
-  B.insert 14 "ytreyrey" tree
+   B.insert 31 "fqweqwe"  $
+   B.insert 43 "ytreyrey" $
+   B.insert 54 "ytreyrey" $
+   B.insert 30 "ytreyrey" $
+   B.insert 17 "ytreyrey" $
+   B.insert 14 "ytreyrey" $ 
+   B.createTree 1 "test"
 
+  
 main :: IO ()
 main = do
   let dummy = creatingADummyIndex
